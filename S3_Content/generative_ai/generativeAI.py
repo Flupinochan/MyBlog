575694af2f8772730d2f74355390b26602904b18
Flupinochan <flupino@metalmental.net>
2024-03-28 08:59:09 +0900

    fix

--- S3_Content/generative_ai/generativeAI.py
+++ S3_Content/generative_ai/generativeAI.py
@@ -2,7 +2,7 @@ import json
 import boto3
 from botocore.config import Config
 
-region = 'us-east-1'
+region = 'ap-northeast-1'
 config = Config(
     region_name=region,
     retries={
@@ -10,17 +10,19 @@ config = Config(
         'max_attempts': 3
     }
 )
-client = boto3.client('bedrock-runtime')
+bedrock_runtime_client = boto3.client('bedrock-runtime')
+bedrock_client = boto3.client('bedrock')
 
-model_id = 'anthropic.claude-3-sonnet-20240229-v1:0'
-
+# model_id = 'anthropic.claude-3-sonnet-20240229-v1:0'
+model_id = 'anthropic.claude-v2:1'
 def lambda_handler(event, context):
+    # print(bedrock_client.list_foundation_models())
     format = {
         'prompt': f'\n\nHuman: {event['prompt']}\nAssistant:',
         'max_tokens_to_sample': 4000
     }
 
-    response = client.invoke_model(
+    response = bedrock_runtime_client.invoke_model(
         body=json.dumps(format),
         contentType='application/json',
         accept='application/json',
@@ -28,7 +30,7 @@ def lambda_handler(event, context):
     )
 
     response_body = json.loads(response.get('body').read())
-    print(response_body.get('completion'))
+    # print(response_body.get('completion'))
     response_json= json.dumps({
         'prompt': response_body.get('completion')
     })
