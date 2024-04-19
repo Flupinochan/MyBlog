8a414f3103ced7ce0acca66c4fe23c46f1a0c179
Flupinochan <flupino@metalmental.net>
2024-04-19 20:21:31 +0900

    fix

--- S3_Content/react/src/App.tsx
+++ S3_Content/react/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Progressbar from "./components/Progressbar";
 import Head from "./components/Head";
 import Title from "./components/Title";
 import Menu from "./components/Menu";
@@ -11,6 +12,7 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <Progressbar />
         <Head />
         <Title />
         <Menu />
---
+++ S3_Content/react/src/components/Progressbar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./Progressbar.css";
+
+const Progressbar: React.FC = () => {
+  return <div id="progressbar"></div>;
+};
+
+export default Progressbar;
