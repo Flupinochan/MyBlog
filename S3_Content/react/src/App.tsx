9dad087a7edd189a0ee8263ba5e3e2feaec51a6d
Flupinochan <flupino@metalmental.net>
2024-04-20 15:13:48 +0900

    Customise scrollbars

--- S3_Content/react/src/App.tsx
+++ S3_Content/react/src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "simplebar-react/dist/simplebar.min.css";
+import SimpleBar from "simplebar-react";
 
 import Progressbar from "./components/Progressbar";
 import Head from "./components/Head";
 import Title from "./components/Title";
 import Menu from "./components/Menu";
 import Home from "./components/Home";
+import "./App.css";
 import Blog20240330 from "./blog/2024/03/30/index";
 import Blog20240418 from "./blog/2024/04/18/index";
 
@@ -17,12 +20,14 @@ function App() {
         <Head />
         <Title />
         <Menu />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/blog/vol1" element={<Blog20240330 />} />
-          <Route path="/blog/vol2" element={<Blog20240418 />} />
-          <Route path="*" element={<Home />} />
-        </Routes>
+        <SimpleBar style={{ maxHeight: "84vh" }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/blog/vol1" element={<Blog20240330 />} />
+            <Route path="/blog/vol2" element={<Blog20240418 />} />
+            <Route path="*" element={<Home />} />
+          </Routes>
+        </SimpleBar>
       </Router>
     </div>
   );
