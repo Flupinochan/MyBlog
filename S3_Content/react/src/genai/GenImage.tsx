fb4f3ff50a1bb806d7b37a6f21f909e1b3721852
Flupinochan <flupino@metalmental.net>
2024-04-27 20:25:04 +0900

    fix

--- S3_Content/react/src/genai/GenImage.tsx
+++ S3_Content/react/src/genai/GenImage.tsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import TextareaAutosize from "react-textarea-autosize";
 import styled from "styled-components";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import ArrowCircleUpIcon from "@mui/icons-material/ArrowCircleUp";
+import axios, { AxiosResponse } from "axios";
+
+import "./GenImage.css";
+
+// TextArea用
+const Container = styled.div`
+  position: relative;
+  width: 100%;
+  height: auto;
+`;
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#4c54c0",
+    },
+  },
+});
 
 const StyledTextarea = styled(TextareaAutosize)`
+  flex-direction: row;
+  align-items: flex-start;
   width: 100%;
-  padding: 10px;
-  font-size: 16px;
+  padding: 13px;
+  font-size: 18px;
   color: #4c54c0;
   background-color: #f7f7f7;
   border: 2px solid #ccc;
@@ -21,36 +44,66 @@ const StyledTextarea = styled(TextareaAutosize)`
   }
 `;
 
-const AutoResizeTextarea: React.FC = () => {
-  const [value, setValue] = React.useState("");
+const StyledButton = styled(Button)`
+  bottom: 48px;
+  right: 7px;
+  float: right;
+`;
+
+// メイン
+const GenImage: React.FC = () => {
+  const [value, setValue] = useState("");
+  const [posiPromptData, setPosiPromptData] = useState("");
 
   const handleChangeValue = (evt: React.ChangeEvent<HTMLTextAreaElement>) => {
     setValue(evt.target.value);
   };
 
-  return (
-    <StyledTextarea
-      value={value}
-      onChange={handleChangeValue}
-      placeholder="Input Prompt..."
-    />
-  );
-};
+  const posiPromptRef = useRef<HTMLTextAreaElement>(null);
 
-const GenImage: React.FC = () => {
+  interface ApiResponse {
+    statusCode: number;
+    body: string;
+  }
+
+  const handleSubmit = () => {
+    axios
+      .get(
+        `https://www.metalmental.net/api/imagegen?positive_prompt=${value}&negative_prompt=none`
+      )
+      .then((response: AxiosResponse<ApiResponse>) => {
+        setPosiPromptData(response.data.body);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div>
       <h2>Generate Image</h2>
       <div className="blogContentBackColor">
         <div className="wrap">
-          <div className="right">
-            <p>test1</p>
-          </div>
           <div className="left">
+            <p>value : {posiPromptData}</p>
+          </div>
+          <div className="right">
             <p>test2</p>
           </div>
         </div>
-        <AutoResizeTextarea />
+        <ThemeProvider theme={theme}>
+          <Container>
+            <StyledTextarea
+              ref={posiPromptRef}
+              value={value}
+              onChange={handleChangeValue}
+              placeholder="Input Prompt..."
+            />
+            <StyledButton variant="contained">
+              <ArrowCircleUpIcon onClick={handleSubmit} fontSize="small" />
+            </StyledButton>
+          </Container>
+        </ThemeProvider>
       </div>
     </div>
   );
