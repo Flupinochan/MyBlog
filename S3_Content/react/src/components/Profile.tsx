e4ee2f13f3e7717aba7e8145a51e3b322279aa46
Flupinochan <flupino@metalmental.net>
2024-06-01 22:00:10 +0900

    fix

--- S3_Content/react/src/components/Profile.tsx
+++ S3_Content/react/src/components/Profile.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from "react";
 import axios from "axios";
-
+import ReactLoading from "react-loading";
 import TextareaAutosize from "react-textarea-autosize";
 
 const Profile: React.FC = () => {
@@ -8,9 +8,11 @@ const Profile: React.FC = () => {
   const emailRef = useRef<HTMLInputElement>(null);
   const messageRef = useRef<HTMLTextAreaElement>(null);
   const [errorMessage, setErrorMessage] = useState<string>("");
+  const [spinner, setSpinner] = useState<true | false>(false);
 
   const handleSubmit = () => {
-    console.log("send start");
+    setErrorMessage("");
+    setSpinner(true);
     const name = nameRef.current!.value;
     const email = emailRef.current!.value;
     const message = messageRef.current!.value;
@@ -29,16 +31,16 @@ const Profile: React.FC = () => {
       axios
         .post(url, postData, postConfig)
         .then((res) => {
-          console.log("send success");
           setErrorMessage("送信しました");
+          setSpinner(false);
         })
         .catch((err) => {
-          console.log("send error");
           setErrorMessage("送信に失敗しました");
+          setSpinner(false);
         });
     } else {
-      console.log("send error");
       setErrorMessage("空欄があります");
+      setSpinner(false);
     }
   };
   return (
@@ -50,11 +52,9 @@ const Profile: React.FC = () => {
           lg:text-7xl lg:pt-40 
           md:text-5xl md:pt-24"
           >
-            TailwindCSSで
+            プロフィール
             <br />
-            美しいサイトを
-            <br />
-            作ります
+            作成中
           </h1>
         </div>
         <img
@@ -73,36 +73,41 @@ const Profile: React.FC = () => {
           <span className="absolute h-1 bg-font-color1 rounded-full w-96 -bottom-5 -left-32" />
         </h2>
       </div>
-      <div className="w-full grid justify-items-center lg:grid-cols-2 gap-8 lg:gap-64 mt-16">
-        <div className="justify-self-end space-y-12 w-72">
+      <div className="w-full grid justify-items-center md:grid-cols-2 gap-8 md:gap-32 lg:gap-64 mt-16">
+        <div className="md:justify-self-end space-y-12 w-72">
           <div>
-            <label className="block mb-5 text-xl font-bold">お名前</label>
+            <label className="block mb-5 text-xl font-bold text-center md:text-left">お名前</label>
             <input ref={nameRef} type="text" className="input-textarea" />
           </div>
           <div>
-            <label className="block mb-5 text-xl font-bold">メールアドレス</label>
+            <label className="block mb-5 text-xl font-bold text-center md:text-left">メールアドレス</label>
             <input ref={emailRef} type="text" className="input-textarea" />
           </div>
           <div>
-            <label className="block mb-5 text-xl font-bold">メッセージ</label>
+            <label className="block mb-5 text-xl font-bold text-center md:text-left">メッセージ</label>
             <TextareaAutosize ref={messageRef} className="input-textarea" />
           </div>
-          {errorMessage && <div className="font-bold text-right">{errorMessage}</div>}
-          <div className="text-right">
+          {spinner && (
+            <div className="flex md:justify-end justify-center">
+              <ReactLoading type={"spin"} color={"#4c54c0"} height={50} width={50} />
+            </div>
+          )}
+          {errorMessage && <div className="font-bold md:text-right text-center">{errorMessage}</div>}
+          <div className="md:text-right text-center">
             <button onClick={handleSubmit} className="bg-font-color1 px-8 py-3 text-white rounded-lg font-bold hover:bg-font-color2 hover:text-font-color1 transition-all duration-300">
               送信する
             </button>
           </div>
         </div>
-        <div className="justify-self-start">
-          <div className="mt-20 space-x-6">
+        <div className="md:justify-self-start">
+          <div className="mt-10 space-x-10">
             <a href="mailto:flupino@metalmental.net">
               <i className="text-3xl fa-solid fa-envelope" />
             </a>
-            <a href="https://github.com/Flupinochan">
+            <a href="https://github.com/Flupinochan" target="_blank" rel="noopener noreferrer">
               <i className="text-3xl fa-brands fa-github" />
             </a>
-            <a href="https://www.youtube.com/@Flupinochan">
+            <a href="https://www.youtube.com/@Flupinochan" target="_blank" rel="noopener noreferrer">
               <i className="text-3xl fa-brands fa-youtube" />
             </a>
           </div>
